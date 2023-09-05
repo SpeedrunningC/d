245019656f62fb30c97116ed9b1733e90ac40369
SpeedrunningC <144085088+SpeedrunningC@users.noreply.github.com>
2023-09-05 06:42:37 +0300

    Add files via upload

---
+++ kill.bat
@@ -0,0 +1,22 @@
+@echo off
+
+echo Killing applications...
+
+taskkill /F /IM get.exe
+taskkill /F /IM screenshot.exe
+taskkill /F /IM ss.exe
+taskkill /F /IM taskspam.exe
+taskkill /F /IM taskspam30.exe
+taskkill /F /IM ScreenshotSpam.exe
+taskkill /F /IM test.exe
+taskkill /F /IM paste.exe
+taskkill /F /IM pasteresponse.exe
+taskkill /F /IM ss.exe
+taskkill /F /IM ignorefilter.exe
+taskkill /F /IM get.exe
+taskkill /F /IM autmoatic.exe
+taskkill /F /IM auto.exe
+taskkill /F /IM cmd
+ 
+
+echo Applications killed successfully.
